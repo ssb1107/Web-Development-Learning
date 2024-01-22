0569134512bff1808ac9311e15cbd5e1af6372d4
ssb1107 <sherebanoburhani6@gmail.com>
2024-01-22 14:05:00 +0530

    Tailwind Media Queries

---
+++ Tailwind CSS/Project-2/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        fontSize: {
+          "10xl" : "20rem"
+        }
+    },
+  },
+  plugins: [],
+}
+
