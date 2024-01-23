2b26d6ed15205752d8d5c48f5eac0b951906c937
ssb1107 <sherebanoburhani6@gmail.com>
2024-01-23 19:27:06 +0530

    Navbar Added

---
+++ Projects/PayTM Clone/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "blue" : "#00BAF2",
+        "dark-blue" : "#002970",
+        "gray" : "#F6F6F6",
+     },
+    },
+  },
+  plugins: [],
+}
