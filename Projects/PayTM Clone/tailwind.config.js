db9c7fae5f39d9177bf46f4b7ce49331ccf8cac2
ssb1107 <sherebanoburhani6@gmail.com>
2024-01-25 18:00:00 +0530

    Section 1 and 2 added

--- Projects/PayTM Clone/tailwind.config.js
+++ Projects/PayTM Clone/tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         "blue" : "#00BAF2",
         "dark-blue" : "#002970",
         "gray" : "#F6F6F6",
+        "hover-color" : "#00afe3",
+        "hover-2-color" : "#0f4a8a"
      },
     },
   },
