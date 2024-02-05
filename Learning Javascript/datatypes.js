73fb6ab49cc7d64bae39e47a40ae66efd97acc3a
ssb1107 <sherebanoburhani6@gmail.com>
2024-02-05 12:30:00 +0530

    Datatypes in JS

---
+++ Learning Javascript/datatypes.js
@@ -0,0 +1,9 @@
+"use strict" // Use new syntax of JS
+let name = "Sherebano";
+let age = 22;
+let isStudying = true;
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudying);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefjned
