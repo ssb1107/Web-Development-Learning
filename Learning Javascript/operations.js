8bcc0852380d83aed82d880dff5117463a463704
ssb1107 <sherebanoburhani6@gmail.com>
2024-02-07 12:45:00 +0530

    Datatype Conversion

---
+++ Learning Javascript/operations.js
@@ -0,0 +1,27 @@
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+let str1 = "hello"
+let str2 = " sherebano";
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1" + 2);
+console.log(2 + "1");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+console.log(2 + 2 + "1" + 5 + 3);
+let valueone = 2 + 2 + "1" + 5 + 3;
+console.log(typeof valueone);
+console.log(+true);
+console.log(-true);
+console.log(1 - true);
+console.log(true - 1);
+let num1 , num2 , num3;
+num1 = num2 = num3 = 5+5;
+console.log(num1 , num2);
