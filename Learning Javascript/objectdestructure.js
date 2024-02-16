6d4c25ac857d5f2ff4d4f7d63bb6361a612c8d85
ssb1107 <sherebanoburhani6@gmail.com>
2024-02-16 12:45:00 +0530

    Object Destructure

---
+++ Learning Javascript/objectdestructure.js
@@ -0,0 +1,12 @@
+const course = {
+    coursename : "Js in hindi",
+    price : 999,
+    instructor : "Hitesh Choudhary"
+}
+const {instructor : teacher} = course;
+console.log(teacher);
+// {
+//     "name" : "Sherebano",
+//     "course" : "Arabic",
+//     "price" : "free",
+// }
