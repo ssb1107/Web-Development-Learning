04bfcc8b6f15179c067e623d6963e39774146c8b
ssb1107 <sherebanoburhani6@gmail.com>
2024-02-18 12:45:00 +0530

    Scope Basics

---
+++ Learning Javascript/scope.js
@@ -0,0 +1,11 @@
+function one(){
+    let username = "sherebano";
+    function two(){
+        let username = "batul";
+        let website = "youtube";
+        console.log(username);
+    }
+    two();
+    console.log(website);
+}
+one();
