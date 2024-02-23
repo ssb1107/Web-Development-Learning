b0adb57501c17c19501213f14963fe433b452ea2
ssb1107 <sherebanoburhani6@gmail.com>
2024-02-23 12:45:00 +0530

    While and do while loop

---
+++ Learning Javascript/whileanddowhile.js
@@ -0,0 +1,17 @@
+let i = 1;
+
+while(i <= 10){
+    console.log(i);
+    i+=2;
+}
+let myarray = ["panipuri" , "noodles" , "manchurian"];
+let j = 0;
+while(j <  myarray.length){
+    console.log(myarray[j]);
+    j++;
+}
+// let k = 0;
+do{
+   console.log("I will be printed once")
+   i++;
+}while(i < 5);
