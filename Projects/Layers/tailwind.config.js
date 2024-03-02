6bf8e099bc99022cd264fc60c7c76087dc87ce7a
ssb1107 <sherebanoburhani6@gmail.com>
2024-03-02 18:00:00 +0530

    Project

--- Projects/Layers/tailwind.config.js
+++ Projects/Layers/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   theme: {
     extend: {
       screens : {
+        'mobile' : {'max' : "420px"},
         'very-small' : {'max' : "750px" },
         'small' : {'max' : "1200px" }
       }
